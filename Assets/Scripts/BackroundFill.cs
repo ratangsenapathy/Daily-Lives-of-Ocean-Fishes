0dfc252e5e5e9727c7e0caf81115c36529f9cfb3
ratansen <ratangsenapathy@gmail.com>
2017-06-19 10:59:03 -0700

    Initial commit

---
+++ Assets/Scripts/BackroundFill.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackroundFill : MonoBehaviour {
+
+	// Use this for initialization
+	private Vector3 scale;
+	private SpriteRenderer sr;
+	void Start () {
+		sr = GetComponent<SpriteRenderer>();
+		ScaleBackgroundToFill ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if (scale != transform.localScale) {
+			ScaleBackgroundToFill ();
+		//}
+	}
+
+	void ScaleBackgroundToFill()
+	{
+		if (sr == null) return;
+		transform.localScale = new Vector3(1,1,1);
+		float width = sr.sprite.bounds.size.x;
+		float height = sr.sprite.bounds.size.y;
+		double worldScreenHeight = Camera.main.orthographicSize * 2.0;
+		double worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+		transform.localScale = new Vector3 ((float)worldScreenWidth / width,(float)worldScreenHeight / height,1.0f);
+		scale = new Vector3 ((float)worldScreenWidth / width, (float)worldScreenHeight / height, 1.0f);
+	}
+}
