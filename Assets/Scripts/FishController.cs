a0d7a835f86d3f450f54fce8a2e06015393c8217
ratansen <ratangsenapathy@gmail.com>
2017-06-20 10:43:27 -0700

    Fishes swimming in correct manner

--- Assets/Scripts/FishController.cs
+++ Assets/Scripts/FishController.cs
@@ -6,13 +6,27 @@ public class FishController : MonoBehaviour {
 
 	// Use this for initialization
 	public bool isFacingRight;
-
+	public float speed;
+	private Vector2 directionVector;
+	private Rigidbody2D rb;
 	void Start () {
-		
+		rb = GetComponent<Rigidbody2D> ();
+		if (isFacingRight)
+			directionVector = new Vector2 (1.0f, 0.0f);
+		else
+			directionVector = new Vector2 (-1.0f, 0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 position = Camera.main.WorldToViewportPoint (transform.position);
+		if ((directionVector.x < 0.0f && position.x <= 0.05f) ||(directionVector.x > 0.0f && position.x >= 0.95f)) {
+			directionVector *= -1;
+			isFacingRight = !isFacingRight;
+			transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+		}
+
+		rb.velocity = speed * directionVector;
+
 	}
 }
